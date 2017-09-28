48c62c84d9b38e2c31e07155c81f445b59f39307
gerardo alfredo alarcon rivas <gerardoalfredo2@gmail.com>
2017-09-28 13:16:42 -0500

    Update to Example 2 The script now can run every of the operation of the Rest Api

--- Exercises/FIrst Flask Application/Examples/Example2.py
+++ Exercises/FIrst Flask Application/Examples/Example2.py
@@ -1,4 +1,6 @@
-from flask import Flask,jsonify
+﻿
+
+from flask import Flask,jsonify,request, render_template
 
 app=Flask(__name__)
 stores=[
@@ -13,18 +15,33 @@ stores=[
         }
     ]
 
-
+@app.route('/')
+def home():
+    return render_template('index.html')
 #Post :-used to receive data
 #Get  :-Used to send data back only
 
 #Post:/store data:{name:}
 @app.route('/store',methods =['POST'])
 def create_store():
-    pass
+    request_data=request.get_json()
+    new_store={
+        'name':request_data['name'],
+        'items':[]}
+    stores.append(new_store)
+    return jsonify(new_store)
 #Get /store/<string:name>
 @app.route('/store/<string:name>')  #'http://127.0.0.1:5000/store/store_name'
 def get_store(name):
-    pass
+    for store in stores:
+        if store['name']==name:
+            return jsonify(store)
+        return jsonify({'message':'store not found'})
+    #Iterate over Stores
+    #if the store name matches, return it
+    #If none match, return an error message
+
+    
 #Get:/store
 @app.route('/store')  #
 def get_stores():
@@ -32,9 +49,22 @@ def get_stores():
 #Post /store/<string:name>/item<name:,price:>
 @app.route('/store/<string:name>/item',methods =['POST'])
 def create_item_in_store(name):
-    pass
+   request_data=request.get_json()
+   for store in stores:
+       if store['name']==name:
+           new_item={
+               'name':request_data['name'],
+               'price':request_data['price']
+               }
+           store['items'].append(new_item)
+           return jsonify(new_item)
+   return jsonify({'message':'store not found'})
+    
 #Get /store/<string:name>/item
 @app.route('/store/<string:name>/item')  #
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name']==name:
+            return jsonify({'items':store['items']})
+    return  jsonify({'message':'store not found'})
 app.run(port=5000)
