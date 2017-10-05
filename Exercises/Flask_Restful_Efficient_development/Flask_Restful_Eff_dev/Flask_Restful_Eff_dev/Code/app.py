a0add92821cb19a17078ae67097a7c12ccbe85ca
gerardo alfredo alarcon rivas <gerardoalfredo2@gmail.com>
2017-10-05 12:38:08 -0500

    Improving to code and error control updates

--- Exercises/Flask_Restful_Efficient_development/Flask_Restful_Eff_dev/Flask_Restful_Eff_dev/Code/app.py
+++ Exercises/Flask_Restful_Efficient_development/Flask_Restful_Eff_dev/Flask_Restful_Eff_dev/Code/app.py
@@ -1,4 +1,4 @@
-﻿from  flask import Flask
+﻿from  flask import Flask,request
 from flask_restful import Resource,Api
 
 app=Flask(__name__)
@@ -8,17 +8,22 @@ items=[]
 
 class Item(Resource):
     def get(self,name):
-        for item in items:
-            if item['name']==name:
-                return item
-        return {'item':None},404
+        item=next(filter(lambda x: x['name']== name,items),None)#take only one element with next, if is empty return a none
+        return {'item':item},200 if item else 404
 
 
     def post(self,name):
-        item={'name':name,'price':12.00}
+        if next(filter(lambda x: x['name']== name,items),None):
+            return {'message':"An item with name '{}' already exists.".format(name)},400
+        data=request.get_json(silent=True)
+        item={'name':name,'price':data['price']}
         items.append(item)
         return item,201
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
 
 api.add_resource(Item,'/item/<string:name>')
-app.run(port=5000)
+api.add_resource(ItemList,'/items')
+app.run(port=5000,debug=True)
      
